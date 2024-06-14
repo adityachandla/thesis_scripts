8a89188fbffd858e1221df975f8909b29145bf22
adityachandla <adityachandla@gmail.com>
2024-06-14 13:41:59 +0200

    Updated thesis script

--- chart_scripts/thesis.py
+++ chart_scripts/thesis.py
@@ -6,6 +6,8 @@ import common
 
 baseline_dir = "../data/final/firstImpl/general"
 final_dir = "../data/final/lastImpl/general"
+final_oz = "../data/final/lastImpl/onezone"
+old_dir = "../data/phase2ParallelPrefetchOZ"
 image_dir = "../images/thesis_images"
 
 def pie_chart(name: str, pie_info: dict[str, int]):
@@ -43,6 +45,51 @@ def chart(name: str, times: dict[str, list[int]], types: list[str],
     plt.savefig(name)
     plt.close()
 
+def create_onezone_charts():
+    parallelism = 1
+    for algo in ['BFS', 'DFS']:
+        a_name = None
+        if algo == "BFS":
+            a_name = "bfsp"
+        else:
+            a_name = "dfspe"
+        oz, _ = common.read_file(f"{final_oz}/s3_{a_name}_{parallelism}_10.txt")
+        general, _ = common.read_file(f"{final_dir}/s3_{a_name}_{parallelism}_10.txt")
+
+        labels = []
+        for query in range(1,8):
+            labels.append(f"Q-{query}\n{(query+1)//2}-Hop")
+
+        times = {"General": [], "One-Zone": []}
+
+        for i in range(7):
+            times["General"].append(common.avg(general[20*i:20*(i+1)]))
+            times["One-Zone"].append(common.avg(oz[20*i:20*(i+1)]))
+
+        chart(f"{image_dir}/{algo}_cmp_oz.png", times, labels, 
+              title=f"Average time ({algo})",
+              ylim_low=1, ylim_high=1_000_000)
+
+def create_parallelism_charts():
+    times_dict = {}
+    for algo in ["bfsp", "dfspe"]:
+        running_times = []
+        for p in [1,4,6,10,20]:
+            times, _ = common.read_file(f"{final_dir}/s3_{algo}_{p}_10.txt")
+            wall_clock_seconds = common.sum_time(times)/p
+            running_times.append(wall_clock_seconds)
+        if algo == "bfsp":
+            times_dict["BFS"] = running_times
+        elif algo == "dfspe":
+            times_dict["DFS"] = running_times
+        else:
+            raise AssertionError()
+
+    chart(f"{image_dir}/parallelism.png", times_dict, 
+          [str(i) for i in parallelisms], scale="linear", ylim_low=0, ylim_high=80,
+          xlabel="Parallelism", ylabel="Wall clock time (seconds)",
+          title="Workload running time")
+
 def get_hits(stats: list[common.Stats]) -> dict[str,int]:
     res = {}
     res["S3 Fetches"] = stats[-1].S3Fetches - stats[0].S3Fetches
@@ -59,6 +106,14 @@ def create_pie_chart():
     _, dfs_stats = common.read_file(f"{final_dir}/s3_dfspe_1_10.txt")
     hits = get_hits(dfs_stats)
     pie_chart(f"{image_dir}/pie_dfs.png", hits)
+
+    _, seq_dfs_stats = common.read_file(f"{old_dir}/s3_dfs_1_10.txt")
+    hits = get_hits(seq_dfs_stats)
+    pie_chart(f"{image_dir}/pie_dfs_seq.png", hits)
+
+    _, seq_bfs_stats = common.read_file(f"{old_dir}/s3_bfs_1_10.txt")
+    hits = get_hits(seq_bfs_stats)
+    pie_chart(f"{image_dir}/pie_bfs_seq.png", hits)
 
 def create_baseline_comparison():
     parallelism = 1
@@ -94,7 +149,9 @@ def create_baseline_comparison():
 
 def main():
     # create_baseline_comparison()
-    create_pie_chart()
+    # create_pie_chart()
+    # create_parallelism_charts()
+    create_onezone_charts()
 
 if __name__ == "__main__":
     main()
